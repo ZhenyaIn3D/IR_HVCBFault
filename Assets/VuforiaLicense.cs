bf2f1a250f82da04df8676e7d68228f4263665a2
AlexanderIn3d-tech <Alexander@in3d-tech.com>
2026-04-26 09:13:26 +0300

    update UI

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVp1R3A1Zi8vLy8vQUFBQm1kU1BMd0dVL2tiZWxpMkNWSHY0WHFHS0FHOFFjYXpoUUdwNC9UeVU2anhtbWhtdlgzVllVYXN6VU8xeUtESDB0Z2EvWmlwUndsQXpXVlVVK3FHdmZYN0d6ekNEWTY2bmpoU20xb2Q1N1VGTC9Mb2hrSTdqcTFjVmFrUDVtSFlKUnVFZVBkcVI4OVExcmVkM2hsZFFQbHNtSHcrT0JoekZRdm53WGpqSXZVVEU2RVQ1RFRuTUh5KytjNVo1emdSd29DTmI0RTZLRlJsODNwY0xpczc3dkRub3Bqa0UyQjF3a2hVYnZWdXQ5VU85Ymkzak42aGhCSjNUTW4xOXlDVlgzTXZGaUM1bHZLZkV6Q0tUMVkvSzNRV1psdXk2d2xHNjQzbGVIOHl1YmRaS1VmdXM3Sk1ETUo5WEhxRlJ3bngrTkRjNS9kc01TZk52cHhQQ3VTZ1NRUUwvV3IvaVY3cVlsWGl1dDdSU1JnYUk=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
